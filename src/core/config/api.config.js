bcc51949c4fac7a7b84adcfec25d0b3509294e19
LuisYepes601 <tucorreo@example.com>
2026-09-01 20:33:39 -0500

    dd

--- src/core/config/api.config.js
+++ src/core/config/api.config.js
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-  BASE_URL: 'http://localhost:8080',
+  BASE_URL: 'https://biblio-nepo-api.onrender.com',
   ENDPOINTS: {
     LIBROS_ADMIN: '/api/v1/libros/admin',
     CATEGORIAS_LIBROS: '/api/v1/categoria-libros',
